c80f7c1834ee6e26885a4dcb3b4a54de7eb89279
candrags <candra.setiawan@bfi.co.id>
2022-07-05 13:56:49 +0700

    fix(format line code): BTAP-1 reformat line code

--- src/main/java/id/co/bfi/bfitaskallocation/service/impl/UserCamundaServiceImpl.java
+++ src/main/java/id/co/bfi/bfitaskallocation/service/impl/UserCamundaServiceImpl.java
@@ -9,16 +9,17 @@ import id.co.bfi.bfitaskallocation.service.UserCamundaService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class UserCamundaServiceImpl implements UserCamundaService{
+public class UserCamundaServiceImpl implements UserCamundaService {
   private String assignTo;
+
   @Override
   public Integer countUserByGroupName(DelegateExecution execution, String groupName) {
     return execution.getProcessEngineServices()
-    .getIdentityService()
-    .createGroupQuery()
-    .groupId(groupName)
-    .list()
-    .size();
+        .getIdentityService()
+        .createGroupQuery()
+        .groupId(groupName)
+        .list()
+        .size();
   }
 
   @Override
@@ -26,55 +27,55 @@ public class UserCamundaServiceImpl implements UserCamundaService{
     List<String> listUserName = new ArrayList<>();
 
     var usersInGroup = execution.getProcessEngineServices().getIdentityService().createUserQuery()
-            .memberOfGroup(groupName).list();
+        .memberOfGroup(groupName).list();
 
-    if(usersInGroup.size() > 0) {
-            for (int i = 0; i < usersInGroup.size(); i++) {
-              String userName = usersInGroup.get(i).getId();
-              listUserName.add(userName);
-          }
+    if (usersInGroup.size() > 0) {
+      for (int i = 0; i < usersInGroup.size(); i++) {
+        String userName = usersInGroup.get(i).getId();
+        listUserName.add(userName);
+      }
 
-          if (listUserName.size() % 2 != 0) {
-              listUserName.add(listUserName.get(1)); // If odd number of user add user index
-          }
+      if (listUserName.size() % 2 != 0) {
+        listUserName.add(listUserName.get(1)); // If odd number of user add user index
+      }
 
-          int numInterval = (listUserName.size() - 1); // Interval round
-          // int halfSize = listUserName.size() / 2;
+      int numInterval = (listUserName.size() - 1); // Interval round
+      // int halfSize = listUserName.size() / 2;
 
-          List<String> participants = new ArrayList<>();
-          participants.addAll(listUserName); // Add user to List and remove the first user
-          participants.remove(0);
+      List<String> participants = new ArrayList<>();
+      participants.addAll(listUserName); // Add user to List and remove the first user
+      participants.remove(0);
 
-          int listUserSize = participants.size();
+      int listUserSize = participants.size();
 
-          for (int itv = 0; itv < numInterval; itv++) {
-              log.info("INTERVAL : " + (itv + 1));
+      for (int itv = 0; itv < numInterval; itv++) {
+        log.info("INTERVAL : " + (itv + 1));
 
-              int userIdx = itv % listUserSize;
+        int userIdx = itv % listUserSize;
 
-              log.info("Compare: " + participants.get(userIdx) + " and " + listUserName.get(0));
+        log.info("Compare: " + participants.get(userIdx) + " and " + listUserName.get(0));
 
-              var countTaskFirst = execution.getProcessEngineServices().getTaskService().createTaskQuery()
-                      .taskAssignee(listUserName.get(0))
-                      .list().size();
+        var countTaskFirst = execution.getProcessEngineServices().getTaskService().createTaskQuery()
+            .taskAssignee(listUserName.get(0))
+            .list().size();
 
-              log.info(listUserName.get(0) + " HAVE " + countTaskFirst + " TASK ");
+        log.info(listUserName.get(0) + " HAVE " + countTaskFirst + " TASK ");
 
-              var countTaskSecond = execution.getProcessEngineServices().getTaskService().createTaskQuery()
-                      .taskAssignee(participants.get(userIdx))
-                      .list().size();
+        var countTaskSecond = execution.getProcessEngineServices().getTaskService().createTaskQuery()
+            .taskAssignee(participants.get(userIdx))
+            .list().size();
 
-              log.info(participants.get(userIdx) + " HAVE " + countTaskSecond + " TASK ");
+        log.info(participants.get(userIdx) + " HAVE " + countTaskSecond + " TASK ");
 
-              if (countTaskFirst < countTaskSecond) {
-                  log.info("Assign: " + listUserName.get(0));
-                  assignTo = listUserName.get(0);
-              } else {
-                  log.info("Assign: " + participants.get(userIdx));
-                  assignTo = participants.get(userIdx);
-              }
+        if (countTaskFirst < countTaskSecond) {
+          log.info("Assign: " + listUserName.get(0));
+          assignTo = listUserName.get(0);
+        } else {
+          log.info("Assign: " + participants.get(userIdx));
+          assignTo = participants.get(userIdx);
+        }
 
-    }
+      }
 
     }
     return assignTo;
