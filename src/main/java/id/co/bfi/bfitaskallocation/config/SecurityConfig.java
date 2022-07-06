fb61d6a8895d28ca20bc0dae585faa7c81112b8a
candrags <candra.setiawan@bfi.co.id>
2022-07-06 17:20:14 +0700

    fix(api authorization): BTAP-8 update SecurityConfig.java

--- src/main/java/id/co/bfi/bfitaskallocation/config/SecurityConfig.java
+++ src/main/java/id/co/bfi/bfitaskallocation/config/SecurityConfig.java
@@ -24,46 +24,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http
-      .csrf()
-      .disable()
-      // make sure we use stateless session; session won't be used to store user's
-      // state.
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      .and()
-      // handle an authorized attempts (if we happen to have any other custom
-      // response)
-      .exceptionHandling()
-      .authenticationEntryPoint((request, response, authError) ->
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
-      )
-      .and()
-      // Add a filter to validate the tokens with every request (service will take
-      // care of that)
-      .addFilterAfter(
-        // This is where we hook our auth filter
-        new AuthenticationFilter(authenticationService),
-        UsernamePasswordAuthenticationFilter.class
-      )
-      // Authorization requests config
-      .authorizeRequests()
-      // Enabled swagger end points
-      .antMatchers("/api-docs", "/api-docs/**", "/configuration/**", "/swagger*/**", "/webjars/**")
-      .permitAll()
-      // Allow OPTIONS call for preflight requests
-      .antMatchers(HttpMethod.OPTIONS)
-      .permitAll()
-      // Allow internal endpoints for service users only !
-      .antMatchers("/internal/", "/internal/**")
-      .hasAuthority(AuthenticationClaim.SYSTEM_SERVICE.toString())
-      // Only registration claim should be able to register
-      .antMatchers(HttpMethod.POST, "/")
-      .hasAuthority(AuthenticationClaim.REGISTRATION.toString())
-      // All others should have LOGIN claim
-      .antMatchers("/**")
-      .hasAuthority(AuthenticationClaim.LOGIN.toString())
-      .anyRequest()
-      .authenticated();
+    http.csrf().disable()
+        // make sure we use stateless session; session won't be used to store user's
+        // state.
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        // handle an authorized attempts (if we happen to have any other custom
+        // response)
+        .exceptionHandling()
+        .authenticationEntryPoint(
+            (request, response, authError) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+        .and()
+        // Add a filter to validate the tokens with every request (service will take
+        // care of that)
+        .addFilterAfter(
+            // This is where we hook our auth filter
+            new AuthenticationFilter(authenticationService), UsernamePasswordAuthenticationFilter.class)
+
+        // Authorization requests config
+        .authorizeRequests()
+
+        // Enabled swagger end points
+        .antMatchers("/api-docs", "/api-docs/**", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+
+        // Allow OPTIONS call for preflight requests
+        .antMatchers(HttpMethod.OPTIONS).permitAll()
+
+        // Allow internal endpoints for service users only !
+        .antMatchers("/**").hasAuthority(AuthenticationClaim.SYSTEM_SERVICE.toString()).anyRequest().authenticated();
   }
 }
