bf69662080868ae45534f32fadf56f1e733dc313
timpamungkas <timotius.pamungkas@bfi.co.id>
2022-07-07 09:42:19 +0700

    feat(generic-api): BTAP-10 generic api exception handler & response

---
+++ src/main/java/id/co/bfi/bfitaskallocation/constant/ApiGenericExceptionHandlerConstants.java
@@ -0,0 +1,13 @@
+package id.co.bfi.bfitaskallocation.constant;
+
+public interface ApiGenericExceptionHandlerConstants {
+
+  String CODE_BAD_REQUEST = "bad_request";
+  String CODE_SQL_EXCEPTION = "sql_exception";
+  String CODE_UNREADABLE_REQUEST_BODY = "unreadable_request_body";
+  String MESSAGE_BAD_INPUT_FROM_CLIENT = "Bad input from client";
+  String MESSAGE_CANNOT_READ_REQUEST = "Cannot read request";
+  String MESSAGE_SQL_EXCEPTION = "SQL exception from server";
+  String REASON_CANNOT_READ_REQUEST_BODY = "Cannot read request body. Check submmited data (e.g. format, content, ...)";
+
+}
