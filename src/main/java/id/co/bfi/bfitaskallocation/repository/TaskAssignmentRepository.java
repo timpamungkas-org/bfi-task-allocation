e0b5e0e500755dbf80d681f817079dcbaaa4a8cd
candrags <candra.setiawan@bfi.co.id>
2022-07-07 11:40:51 +0700

    fix(refactor): BTAP-7 change JpaRepo to CrudRepo

--- src/main/java/id/co/bfi/bfitaskallocation/repository/TaskAssignmentRepository.java
+++ src/main/java/id/co/bfi/bfitaskallocation/repository/TaskAssignmentRepository.java
@@ -1,8 +1,8 @@
 package id.co.bfi.bfitaskallocation.repository;
 
 import id.co.bfi.bfitaskallocation.entity.TaskAssignment;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskAssignmentRepository extends JpaRepository<TaskAssignment, Long> {}
+public interface TaskAssignmentRepository extends CrudRepository<TaskAssignment, Long> {}
