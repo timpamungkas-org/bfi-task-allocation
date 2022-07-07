ca1efacfa87474aaa1c0605c38ddffd734d9a4dc
timpamungkas <timotius.pamungkas@bfi.co.id>
2022-07-07 12:27:01 +0700

    fix(code-smell) : add public static final

--- src/main/java/id/co/bfi/bfitaskallocation/constant/ApiGenericExceptionHandlerConstants.java
+++ src/main/java/id/co/bfi/bfitaskallocation/constant/ApiGenericExceptionHandlerConstants.java
@@ -2,12 +2,12 @@ package id.co.bfi.bfitaskallocation.constant;
 
 public final class ApiGenericExceptionHandlerConstants {
 
-  String CODE_BAD_REQUEST = "bad_request";
-  String CODE_SQL_EXCEPTION = "sql_exception";
-  String CODE_UNREADABLE_REQUEST_BODY = "unreadable_request_body";
-  String MESSAGE_BAD_INPUT_FROM_CLIENT = "Bad input from client";
-  String MESSAGE_CANNOT_READ_REQUEST = "Cannot read request";
-  String MESSAGE_SQL_EXCEPTION = "SQL exception from server";
-  String REASON_CANNOT_READ_REQUEST_BODY = "Cannot read request body. Check submmited data (e.g. format, content, ...)";
+  public static final String CODE_BAD_REQUEST = "bad_request";
+  public static final String CODE_SQL_EXCEPTION = "sql_exception";
+  public static final String CODE_UNREADABLE_REQUEST_BODY = "unreadable_request_body";
+  public static final String MESSAGE_BAD_INPUT_FROM_CLIENT = "Bad input from client";
+  public static final String MESSAGE_CANNOT_READ_REQUEST = "Cannot read request";
+  public static final String MESSAGE_SQL_EXCEPTION = "SQL exception from server";
+  public static final String REASON_CANNOT_READ_REQUEST_BODY = "Cannot read request body. Check submmited data (e.g. format, content, ...)";
 
 }
